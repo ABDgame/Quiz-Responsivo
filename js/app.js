de23d17498689e999aba6170f8fdb28d23e2429f
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-29 20:39:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
+const questionNumber = document.querySelector(".question-number");
+
+const questionText = document.querySelector(".question-text");
+
+const optionContainer = document.querySelector("option-container");
+
+let questionContainer = 0;
+let currentQuestion;
+let avaliableQuestion = [  ];
+
+//push the questions into avaliableQuestions Array
+
+function setAvaliableQuestions( ){
+    const totalQuestion = quiz.length;
+    for(let i=0; i<totalQuestion; i++){
+   avaliable 
+
+ }
+}
+
+window.onload = function( ){
+
+   setAvaliableQuestions( );
+}
 

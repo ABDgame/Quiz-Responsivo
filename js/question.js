050e6de7aec2e9a43f52f1ca437b78074466f11f
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2023-06-29 20:45:14 -0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,1 +1,140 @@
+// Array of Objects
+
+const quiz = [
+ {
+    q: 'Imagem do ABACAXI  ' ,
+    options:[ 'AMORA','ABACAXI','ABACATE','AÇAI'],
+    answer:1
+   },
+
+
+{
+    q: Imagem da ESCADA ,
+    options:[ "EMA","ESTOJO","ELEFANTE","ESCADA" ],
+    answer:3
+   },
+
+
+{
+    q: Imagem do ÍNDIO ,
+    options:[ "ÍNDIO","IOGURTE","IGREJA","IMÃ" ],
+    answer:0
+   },
+
+
+{
+    q: Imagem do Urso ,
+    options:[ "UVA","UNHA","URUBU","URSO" ],
+    answer:3
+   },
+
+
+{
+    q: Imagem da Orelha,
+    options:[ "OITO","OCELHA","ORELHA","OURO" ],
+    answer:2
+   },
+
+
+{
+    q: Imagem da ARANHA ,
+    options:[ "ÁRVORE","ANEL","ARANHA","ASTRONAUTA" ],
+    answer:2
+   }, 
+
+
+{
+    q: Imagem da ESTRELA,
+    options:[ "ESCOVA","ELEFANTE","ESPINHO","ESTRELA" ],
+    answer:3
+   },
+
+
+{
+    q: Imagem do ÓCULOS,
+    options:[ "OLHOS","ÓCULOS","OURO","OITO" ],
+    answer:1
+   },
+
+
+{
+    q: Imagem da ILHA ,
+    options:[ "ÍNDIO","ILHA","IATE","IMÃ" ],
+    answer:1
+   },
+
+
+{
+    q: Imagem da HIENA ,
+    options:[ "HOJE","HIPIPOTAMO","HOMEM","HIENA" ],
+    answer:3
+   },
+
+
+{
+    q: Imagem da HARPA ,
+    options:[ "HORA","ILHA","HARPA","HIENA" ],
+    answer:2
+   },
+
+
+{
+    q: Imagem da HORA ,
+    options:[ "HORA","HORTA","HOMEM","HIENA" ],
+    answer:0
+   },
+
+
+{
+    q: Imagem da HORTA ,
+    options:[ "HORTELÃ","HARPA","HORA","HORTA" ],
+    answer:3
+   },
+
+{
+    q: Imagem da HOSPITAL,
+    options:[ "HINO","HOSPITAL","HIENA","HOMEM" ],
+    answer:1
+   },
+
+
+{
+    q: Imagem da HOTEL ,
+    options:[ "HOTEL","HOMEM","HARPA","HIENA" ],
+    answer:0
+   },
+
+
+{
+    q: Imagem da HELICÓPTERO,
+    options:[ "HERCULES","HIENA","HINO","HÉRCULES" ],
+    answer:3
+   },
+
+
+{
+    q: Imagem da HORTELÃ  ,
+    options:[ "HELICOPTERO","HOJE","HORTELÃ","HORA" ],
+    answer:1
+   },
+
+
+{
+    q: Imagem da JIPE ,
+    options:[ "JILÓ","JIPE","JOGO","JÓIA" ],
+    answer:1
+   },
+
+
+{
+    q: Imagem da JÓIA  ,
+    options:[ 'JOIA','JOANINHA','JUJUBA','JOGADOR'],
+    answer:0
+   },
+
+]
+
+
+
+
 

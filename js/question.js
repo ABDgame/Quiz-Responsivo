c4710845bc6af955407566b7327ca09d0dd0cfc5
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-13 19:01:47 -0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -4,134 +4,104 @@ const quiz = [
  {
     q: 'Qual o meu nome?',
     options:[ 'AMORA','ABACAXI','ABACATE','AÇAI'],
-    answer:1
-  
+    answer:1 
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "EMA","ESTOJO","ELEFANTE","ESCADA" ],
+    options:['EMA','ESTOJO','ELEFANTE','ESCADA'],
     answer:3
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "ÍNDIO","IOGURTE","IGREJA","IMÃ" ],
+    options:['ÍNDIO','IOGURTE','IGREJA','IMÃ'],
     answer:0
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "UVA","UNHA","URUBU","URSO" ],
+    options:['UVA','UNHA','URUBU','URSO'],
     answer:3
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "OITO","OCELHA","ORELHA","OURO" ],
+    options:['OITO','OVELHA','ORELHA','OURO'],
     answer:2
    },
 
 
 {
     q: 'Qual o meu nome?',
-    options:[ "ÁRVORE","ANEL","ARANHA","ASTRONAUTA" ],
+    options:['ÁRVORE','ANEL','ARANHA','ASTRONAUTA'],
     answer:2
    }, 
 
 
 {
     q: 'Qual o meu nome?',
-    options:[ "ESCOVA","ELEFANTE","ESPINHO","ESTRELA" ],
+    options:['ESCOVA','ELEFANTE','ESPINHO','ESTRELA'],
     answer:3
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "OLHOS","ÓCULOS","OURO","OITO" ],
+    options:['OLHOS','ÓCULOS','OURO','OITO'],
+    answer:1
+   },
+{
+    q: 'Qual o meu nome?',
+    options:['ÍNDIO','ILHA','IATE','IMÃ'],
     answer:1
    },
 
 
 {
     q: 'Qual o meu nome?',
-    options:[ "ÍNDIO","ILHA","IATE","IMÃ" ],
-    answer:1
-   },
-
-
-{
-    q: 'Qual o meu nome?',
-    options:[ "HOJE","HIPIPOTAMO","HOMEM","HIENA" ],
+    options:['HOJE','HIPIPOTAMO','HOMEM','HIENA'],
     answer:3
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HORA","ILHA","HARPA","HIENA" ],
+    options:['HORA','ILHA','HARPA','HIENA'],
     answer:2
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HORA","HORTA","HOMEM","HIENA" ],
+    options:['HORA','HORTA','HOMEM','HIENA'],
     answer:0
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HORTELÃ","HARPA","HORA","HORTA" ],
+    options:['HORTELÃ','HARPA','HORA','HORTA'],
     answer:3
    },
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HINO","HOSPITAL","HIENA","HOMEM" ],
+    options:['HINO','HOSPITAL','HIENA','HOMEM'],
     answer:1
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HOTEL","HOMEM","HARPA","HIENA" ],
+    options:['HOTEL','HOMEM','HARPA','HIENA'],
     answer:0
    },
-
-
 {
     q: '3 + 4 = 7 ?',
     options:[ 'true','false'],
     answer:0
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "HELICOPTERO","HOJE","HORTELÃ","HORA" ],
+    options:['HELICOPTERO','HOJE','HORTELÃ','HORA'],
     answer:1
    },
-
-
 {
     q: 'Qual o meu nome?',
-    options:[ "JILÓ","JIPE","JOGO","JÓIA" ],
+    options:[ 'JILÓ','JIPE','JOGO','JÓIA'],
     answer:1
    },
-
-
 {
     q: 'Qual o meu nome?',
     options:[ 'JOIA','JOANINHA','JUJUBA','JOGADOR'],
     answer:0
-   },
+   }
 
 ]
 

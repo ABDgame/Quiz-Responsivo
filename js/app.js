31fd65a7de0b8e1c0a681b91414c06f338725ba2
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-13 16:22:42 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let questionContainer = 0;
 let currentQuestion;
 let avaliableQuestion = [  ];
 
-//coloca as perguntas no array availableQuestions 
+//Coloca as perguntas no array availableQuestions 
 
 function setAvailableQuestions( ){
     const totalQuestion = quiz.length;
@@ -16,7 +16,7 @@ function setAvailableQuestions( ){
         availableQuestions.push(quiz[i]) 
  }
 }
-//Definir o número da pergunta e as opções das perguntas
+// Defini o número da pergunta e as opções das perguntas
 function getNewQuestion(){
    // Definir o número da pergunta
    questionNumber.innerHTML = " Questão " + (questionCounter + 1) + " de " + quiz.length;
@@ -24,9 +24,9 @@ function getNewQuestion(){
     
 }
 window.onload = function(){
-   // primeiro vai definir todas as perguntas no array availableQuestions
+   // Primeiro vai definir todas as perguntas no array availableQuestions
    setAvailableQuestions();
-   // segundo vai ligar para obter o valor da função getNewQuestion;
+   // Segundo vai ligar para obter o valor da função getNewQuestion;
    getNewQuestion();
 }
 

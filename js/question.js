32319439699c0b7e6527b83104a4541cb00cf1a4
Débora Inocêncio <30512134+ABDgame@users.noreply.github.com>
2024-12-19 13:26:01 -0300

    Update question.js

--- js/question.js
+++ js/question.js
@@ -2,77 +2,77 @@
 
 const quiz = [
  {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:[ 'COCO','COPO','COLA','CORAL'],
     answer:1 
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['CALA','CAMA','CASA','CAJU'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['COLA','CABO','COELHO','CALO'],
     answer:0
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['CASA','CAMELO','CADEIRA','CADEADO'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['CABIDE','CABEÇA','CASA','CANETA'],
     answer:2
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual  é o meu nome?',
     options:['COLHER','CONCHA','CUECA','CUCA'],
     answer:2
    }, 
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['COUVE','COCO','COELHO','COBERTA'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['CAJU','CAMA','CASA','CAIXA'],
     answer:1
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['COCO','COCA','COLA','COPO'],
     answer:1
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['CASA','BALA','FAIXA','CAIXA'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DONA','DADO','DEDO','DENTE'],
     answer:2
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DOCE','DAMA','DADO','DOMINÓ'],
     answer:0
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DATA','DUELO','DAMA','DADO'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DOIDO','DOMINÓ','DONA','DOCE'],
     answer:1
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DINHEIRO','DIA','DIAMANTE','DENTE'],
     answer:0
    },
@@ -82,22 +82,23 @@ const quiz = [
     answer:0
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:['DEDO','DENTE','DIA','DUELO'],
     answer:1
    },
 {
-    q: 'Qual o meu nome?',
-    options:[ 'DIAMANTE','DIA','DEDO','DEDAL'],
-    answer:1
+    q: 'Qual é o meu nome?',
+    options:[ 'PINCEL','PIZZA','PIPA','PIPOCA'],
+    answer:1,
+    img:'img/PIZZA.png'
    },
  {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:[ 'FERRO','FELIZ','FADA','FEIJÃO'],
     answer:3
    },
 {
-    q: 'Qual o meu nome?',
+    q: 'Qual é o meu nome?',
     options:[ 'DIAMANTE','DEDAL','DITADO','DIA'],
     answer:0
    }
